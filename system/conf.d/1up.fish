0791105aa266bbc059b54521a1b3b6d3021f5a65
Adam Cellon <adam.cellon@1up.health>
2023-01-30 16:14:51 -0500

    Create pgdev function(s)

--- system/conf.d/1up.fish
+++ system/conf.d/1up.fish
@@ -1,3 +1,5 @@
+# Abbreviations
+
 abbr -a meet "open https://meet.google.com"
 abbr -a gmail "open https://mail.google.com/mail/u/0/\#inbox"
 abbr -a jira "open https://1uphealth.atlassian.net/jira/software/projects/AUTH/boards/45"
@@ -14,3 +16,10 @@ abbr -a authservice "nvmo ~/work/internal/apps/api/authService"
 abbr -a systemservice "nvmo ~/work/internal/apps/api/systemService"
 abbr -a userservice "nvmo ~/work/internal/apps/api/userService"
 abbr -a authintegrations "nvmo ~/work/auth-integrations/1up-auth-integration-template"
+
+# Completions
+complete -c pgdev -f
+complete -c pgdev -a "connect-only" -d "Connect via ssh to '1uphealthdev' PG database"
+complete -c pgdev -a "open" -d "Open pgAdmin4 and copy password to clipboard"
+complete -c pgdev -a "close" -d "Close ssh conncetion to '1uphealthdev' PG database"
+complete -c pgdev -a "connect" -d "Connect via ssh to '1uphealthdev' PG and open pgAdmin4"
