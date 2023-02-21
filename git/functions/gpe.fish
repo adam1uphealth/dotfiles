2d11a99c9cd1dbc30bd1832257de696d3b1293f7
Adam Cellon <adam.cellon@1up.health>
2023-02-21 16:56:43 -0500

    Add simple gpe function

--- git/functions/gpe.fish
+++ git/functions/gpe.fish
@@ -1,4 +1,4 @@
 function gpe -d "push an empty commit"
-  git commit --allow-empty -m 'Empty commit';
+  git commit --allow-empty -m 'Empty commit to retrigger GitHub actions';
     and git push
 end
